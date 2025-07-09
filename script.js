5b1c070cb1dbd6bd55186dc4f558fa90aa2f9d5f
f1futbol <f1futbolstore@gmail.com>
2025-07-09 00:48:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,42 +32,38 @@ let galeriaActual = [];
     }
   }
     // Filtro por equipo
-  function filtrar(categoria) {
-    const productos = document.querySelectorAll(".producto");
-    const bienvenida = document.getElementById("bienvenida");
-    const logoFixed = document.getElementById("logo-fixed");
-    const titulo = document.getElementById("titulo");
-    const medidas = document.getElementById("medidas");
-    const submenu = document.getElementById("submenu-version");
+function filtrar(categoria) {
+  const productos = document.querySelectorAll(".producto");
+  const bienvenida = document.getElementById("bienvenida");
+  const logoFixed = document.getElementById("logo-fixed");
+  const medidas = document.getElementById("medidas");
+  const submenu = document.getElementById("submenu-version");
 
-    // Cierra el menú de versiones si está abierto
-    submenu.classList.add("hidden");
+  // Cierra el menú de versiones si está abierto
+  submenu.classList.add("hidden");
 
-    productos.forEach(p => {
-      if (categoria === "ninguno" || categoria === "medidas") {
-        p.classList.add("hidden");
-      } else {
-        p.classList.toggle("hidden", !p.classList.contains(categoria));
-      }
-    });
-
-    if (categoria === "ninguno") {
-      bienvenida.style.display = "block";
-      logoFixed.style.display = "none";
-      titulo.classList.remove("titulo-chico");
-      medidas.classList.add("hidden");
-    } else if (categoria === "medidas") {
-      bienvenida.style.display = "none";
-      logoFixed.style.display = "block";
-      titulo.classList.add("titulo-chico");
-      medidas.classList.remove("hidden");
+  productos.forEach(p => {
+    if (categoria === "ninguno" || categoria === "medidas") {
+      p.classList.add("hidden");
     } else {
-      bienvenida.style.display = "none";
-      logoFixed.style.display = "block";
-      titulo.classList.add("titulo-chico");
-      medidas.classList.add("hidden");
+      p.classList.toggle("hidden", !p.classList.contains(categoria));
     }
+  });
+
+  if (categoria === "ninguno") {
+    bienvenida.style.display = "block";
+    logoFixed.style.display = "none";
+    medidas.classList.add("hidden");
+  } else if (categoria === "medidas") {
+    bienvenida.style.display = "none";
+    logoFixed.style.display = "block";
+    medidas.classList.remove("hidden");
+  } else {
+    bienvenida.style.display = "none";
+    logoFixed.style.display = "block";
+    medidas.classList.add("hidden");
   }
+}
   window.onload = () => {
     filtrar("ninguno");
   };
